3ff9cc4b2c10973beb30aeb8207ce6b1bb26fb03
Michael Baker <michael.a.baker34@gmail.com>
2021-08-21 17:27:43 -0500

    add type conversion

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -6,34 +6,78 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
-            byte number = 0;
-            int count = 10;
-            float total = 20.20f;
-            char character = 'a';
-            string firstName = "Joe";
-            bool isOn = true;
-
+            /*  byte number = 0;
+                int count = 10;
+                float total = 20.20f;
+                char character = 'a';
+                string firstName = "Joe";
+                bool isOn = true;
             /*
-            var number = 0;
-            var count = 10;
-            var total = 20.20f;
-            var character = 'a';
-            var firstName = "Joe";
-            var isOn = true;
+                var number = 0;
+                var count = 10;
+                var total = 20.20f;
+                var character = 'a';
+                var firstName = "Joe";
+                var isOn = true;
             */
+            /*  Console.WriteLine(number);
+                Console.WriteLine(count);
+                Console.WriteLine(total);
+                Console.WriteLine(character);
+                Console.WriteLine(firstName);
+                Console.WriteLine(isOn);
+            */
+            /*  // format string
+                const float Pi = 3.14f;
+                Console.WriteLine("byte: {0} - {1}", byte.MinValue, byte.MaxValue);
+                Console.WriteLine("float: {0} - {1}", float.MinValue, float.MaxValue);
+                Console.WriteLine("{0}", Pi);
+            */
+            /*
+                // type conversion
+                // cast int to byte
+                int i = 1;
+                byte b = (byte)i;
 
+                // cast float to int
+                float f = 1.0f;
+                int n = (int)f;
 
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(total);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isOn);
+                // cast string to int
+                string s = "s";
+                int j = Convert.ToInt32(s);
+                int k = int.Parse(s);
 
+                byte b = 1;
+                int i = b;
+                Console.WriteLine(i);
+            
+                string number = "1234";
+                int i = Convert.ToInt32(number);
+                Console.WriteLine(i);
 
-            // format string
-            Console.WriteLine("byte: {0} - {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("float: {0} - {1}", float.MinValue, float.MaxValue);
+                try
+                {
+                    string letters = "asdf";
+                    byte b = Convert.ToByte(letters);
+                    Console.WriteLine(b);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("letters cannot convert to byte");
+                }
+
+                try
+                {
+                    string str = "true";
+                    bool b = Convert.ToBoolean(str);
+                    Console.WriteLine(b);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("str cannot convert to boolean");
+                }
+            */
         }
     }
 }
