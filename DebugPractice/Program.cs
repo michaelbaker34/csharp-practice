3dba24839353cbf13db173053cdaa66d86e5cee0
Michael Baker <michael.a.baker34@gmail.com>
2021-08-22 22:45:16 -0500

    add debugpractice

---
+++ DebugPractice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebugPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            List<int> smallests = GetSmallests(numbers, 3);
+
+            foreach (int number in smallests)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            List<int> smallests = new List<int>(list);
+
+            while (smallests.Count < count)
+            {
+                int min = GetSmallest(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // assumes first number is smallest
+            int min = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] > min)
+                {
+                    min = list[i];
+                }
+                
+            }
+            return min;
+        }
+    }
+}
