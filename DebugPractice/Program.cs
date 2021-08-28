503f6adde9a5952db9d36f2d9fd52e96f471d8ef
Michael Baker <michael.a.baker34@gmail.com>
2021-08-27 20:04:20 -0500

    add argumentoutofrange exception to getSmallests

--- DebugPractice/Program.cs
+++ DebugPractice/Program.cs
@@ -7,10 +7,10 @@ namespace DebugPractice
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6};
+            List<int> numbers = new List<int> {1, 2};
             List<int> smallests = GetSmallests(numbers, 3);
 
-            foreach (int number in numbers)
+            foreach (int number in smallests)
             {
                 // expected output "1, 2, 3, 4, 5, 6"
                 Console.WriteLine(number);
@@ -20,6 +20,11 @@ namespace DebugPractice
 
         public static List<int> GetSmallests(List<int> list, int count)
         {
+            if (count > list.Count)
+            {
+                throw new ArgumentOutOfRangeException("count", "count is greater than number of elements in list");
+            }
+
             List<int> smallests = new List<int>();
             List<int> bufferList = new List<int>(list);
 
