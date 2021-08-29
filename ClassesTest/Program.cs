03220773982248ce7e367bac49510d6fa8e5447c
Michael Baker <michael.a.baker34@gmail.com>
2021-08-28 21:58:48 -0500

    add classes test with introduce method

---
+++ ClassesTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassesTest
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
