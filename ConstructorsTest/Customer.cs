48eabb970e46d09057be6775a5b83f99d6355973
Michael Baker <michael.a.baker34@gmail.com>
2021-08-30 23:57:19 -0500

    add customer class with constructors

---
+++ ConstructorsTest/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConstructorsTest
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+
+
+        // default constructor
+        public Customer()
+        {
+        }
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        // constructor overload
+        public Customer(int id, string name)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+
+    }
+}
--- ConstructorsTest/Program.cs
+++ ConstructorsTest/Program.cs
@@ -6,7 +6,9 @@ namespace ConstructorsTest
     {
         static void Main(string[] args)
         {
-            
+            var customer = new Customer();
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
