0e128d05350a8eb445922d3ad6fe91a6ccebf84a
Michael Baker <michael.a.baker34@gmail.com>
2021-09-04 18:04:29 -0500

    add point class with point methods. create new point in main with log

---
+++ MethodsPractice/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace MethodsPractice
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            this.x = newLocation.x;
+            this.y = newLocation.y;
+        }
+    }
+}
---
+++ MethodsPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+
+            point.Move(new Point(40, 60));
+
+
+            Console.WriteLine("point is at ({0}, {1})", point.x, point.y);
+        }
+    }
+}
