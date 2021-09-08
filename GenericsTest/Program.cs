d1fe984341d0e321c78341e721bbce2f738d1c99
Michael Baker <michael.a.baker34@gmail.com>
2021-09-08 17:54:21 -0500

    add new() constraint to utilities

--- GenericsTest/Program.cs
+++ GenericsTest/Program.cs
@@ -18,6 +18,7 @@ namespace GenericsTest
             //var dictionary = new GenericDictionary<string, Book>();
             //dictionary.Add("1234", new Book());
 
+            //System.Nullable
             var number = new Nullable<int>(5);
             Console.WriteLine("Has Value ? " + number.HasValue);
             Console.WriteLine("Value: " + number.GetValueOrDefault());
--- GenericsTest/Utilities.cs
+++ GenericsTest/Utilities.cs
@@ -12,11 +12,16 @@ namespace GenericsTest
     *  where T : class
     *  where T : new()
     */
-    public class Utilities<T> where T : IComparable
+    public class Utilities<T> where T : IComparable, new()
     {
         public T Max(T a, T b)
         {
             return a.CompareTo(b) > 0 ? a : b;
+        }
+
+        public void DoSomething(T value)
+        {
+            var obj = new T();
         }
     }
 }
