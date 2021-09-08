d482aa078d9bf4d0fa7eda2e812602256a967531
Michael Baker <michael.a.baker34@gmail.com>
2021-09-08 17:25:03 -0500

    add utilities with generic method

---
+++ GenericsTest/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsTest
+{
+    public class Utilities<T> where T : IComparable
+    {
+        public T Max<T>(T a, T b) where T : IComparable
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+}
