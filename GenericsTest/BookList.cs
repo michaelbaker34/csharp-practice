37d9aa2b51d2f19fc87c278390ca4b2d1f8323e5
Michael Baker <michael.a.baker34@gmail.com>
2021-09-08 16:45:38 -0500

    add booklist

---
+++ GenericsTest/BookList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsTest
+{
+    public class BookList
+    {
+        public void Add(BookList book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookList this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public class GenericList<T>
+        {
+            public void Add(T value)
+            {  
+            
+            }
+
+            public T this[int index]
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+    }
+}
