26e9dd53a2c293fb122f88e13fb9c7713ce0aa5b
Michael Baker <michael.a.baker34@gmail.com>
2021-09-10 00:01:05 -0500

    add mailservice subscriber

--- DelegatesPractice/Program.cs
+++ DelegatesPractice/Program.cs
@@ -7,9 +7,21 @@ namespace DelegatesPractice
         static void Main(string[] args)
         {
             var video = new Video() { Title = "Video 1" };
+            
+            // publisher
             var videoEncoder = new VideoEncoder();
+            //subscriber
+            var mailService = new MailService();
 
             videoEncoder.Encode(video);
+        }
+
+        public class MailService
+        {
+            public void OnVideoEncoded(object source, EventArgs e)
+            {
+                Console.WriteLine("MailService: Sending email...");
+            }
         }
     }
 }
